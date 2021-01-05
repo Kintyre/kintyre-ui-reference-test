b32bfecac15d8de17182258121fee019769be5dd
Sandro Braidotti <alessandro.braidotti@gmail.com>
2021-01-05 16:45:02 -0500

    try capitalizing routes file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
-import routes from 'src/routes';
+import routes from 'src/Routes';
 
 const App = () => {
   const routing = useRoutes(routes);
